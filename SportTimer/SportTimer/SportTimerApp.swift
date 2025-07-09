a912d7fdcb84a69c42322d569a8c9b10f5698aa8
acidsasha <141730867+FilovetsAlexandr@users.noreply.github.com>
2025-07-09 16:18:13 +0300

    initial_commit

---
+++ SportTimer/SportTimer/SportTimerApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct SportTimerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
